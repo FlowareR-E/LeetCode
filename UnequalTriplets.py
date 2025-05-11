931711569281c845ba210da42faed267f58973f2
FlowareR-E <rafael.flores@encora.com>
2025-05-11 12:07:45 -0600

    Update UnequalTriplets.py
    Gh desktop shows "file is empty"

---
+++ UnequalTriplets.py
@@ -0,0 +1,14 @@
+def countTriplets(nums):
+    n = len(nums)
+    count = 0
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] == nums[j]:
+                continue
+            for k in range(j + i, n):
+                if nums[i] != nums[k] and nums[j] != nums[k]:
+                    count += 1
+    return count
+
+# Note: this method is very time complex n^3 
